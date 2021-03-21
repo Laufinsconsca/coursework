16b46b2aba8311270f50f8e11e2d82c426057081
Laufinsconsca <walle.log@yandex.ru>
2021-03-22 02:57:04 +0400

    Added the ability to customize J and K separately for each scheme

--- src/main/java/ui/EpsTableController.java
+++ src/main/java/ui/EpsTableController.java
@@ -12,8 +12,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-import javafx.util.Pair;
 import matrices.matrix.Matrix;
 import method.Calculator;
 import org.apache.poi.ss.usermodel.Row;
@@ -34,20 +32,15 @@ public class EpsTableController implements Initializable, aWindow, InputDataDtoH
     private ObservableList<CrankNicolsonSchemeEpsTableRow> crankNicolsonSchemeEpsTableObservableRows;
     public TableView<ImplicitSchemeEpsTableRow> implicitSchemeTableView;
     public TableView<CrankNicolsonSchemeEpsTableRow> crankNicolsonSchemeTableView;
-    public TableColumn<ImplicitSchemeEpsTableRow, Integer> implicitSchemeTableColumnJ;
-    public TableColumn<ImplicitSchemeEpsTableRow, Integer> implicitSchemeTableColumnK;
-    public TableColumn<ImplicitSchemeEpsTableRow, Double> implicitSchemeTableColumnΕ_2h_r_4h_z;
-    public TableColumn<ImplicitSchemeEpsTableRow, Double> implicitSchemeTableColumnΕ_h_r_h_z;
-    public TableColumn<ImplicitSchemeEpsTableRow, Double> implicitSchemeTableColumnΔ_h_r_h_z;
-    public TableColumn<CrankNicolsonSchemeEpsTableRow, Integer> crankNicolsonSchemeTableColumnJ;
-    public TableColumn<CrankNicolsonSchemeEpsTableRow, Integer> crankNicolsonSchemeTableColumnK;
-    public TableColumn<CrankNicolsonSchemeEpsTableRow, Double> crankNicolsonSchemeTableColumnΕ_2h_r_2h_z;
-    public TableColumn<CrankNicolsonSchemeEpsTableRow, Double> crankNicolsonSchemeTableColumnΕ_h_r_h_z;
-    public TableColumn<CrankNicolsonSchemeEpsTableRow, Double> crankNicolsonSchemeTableColumnΔ_h_r_h_z;
+    public TableColumn<ImplicitSchemeEpsTableRow, Integer> implicitSchemeTableColumnJ, implicitSchemeTableColumnK;
+    public TableColumn<ImplicitSchemeEpsTableRow, Double> implicitSchemeTableColumnΕ_2h_r_4h_z, implicitSchemeTableColumnΕ_h_r_h_z, implicitSchemeTableColumnΔ_h_r_h_z;
+    public TableColumn<CrankNicolsonSchemeEpsTableRow, Integer> crankNicolsonSchemeTableColumnJ, crankNicolsonSchemeTableColumnK;
+    public TableColumn<CrankNicolsonSchemeEpsTableRow, Double> crankNicolsonSchemeTableColumnΕ_2h_r_2h_z, crankNicolsonSchemeTableColumnΕ_h_r_h_z, crankNicolsonSchemeTableColumnΔ_h_r_h_z;
     private Stage stage;
     private InputDataDto inputDataDto;
     private double R, L;
-    private Map<Integer, Integer> kj;
+    private Map<Integer, Integer> implicitSchemeKJ;
+    private Map<Integer, Integer> crankNicolsonSchemeKJ;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -72,17 +65,14 @@ public class EpsTableController implements Initializable, aWindow, InputDataDtoH
     public void setStage(Stage stage) {
         stage.setResizable(false);
         stage.setOnShown(windowEvent -> {
-            kj = getKJ();
             R = inputDataDto.getR();
             L = inputDataDto.getL();
             List<ImplicitSchemeEpsTableRow> implicitSchemeEpsTableRows = new ArrayList<>();
             List<CrankNicolsonSchemeEpsTableRow> crankNicolsonSchemeEpsTableRows = new ArrayList<>();
-
-            for (Map.Entry<Integer, Integer> kjEntry : kj.entrySet()) {
-                Pair<ImplicitSchemeEpsTableRow, CrankNicolsonSchemeEpsTableRow> rows = populateRow(inputDataDto.toBuilder().J(kjEntry.getKey()).K(kjEntry.getValue()).build());
-                implicitSchemeEpsTableRows.add(rows.getKey());
-                crankNicolsonSchemeEpsTableRows.add(rows.getValue());
-            }
+            implicitSchemeKJ = getJK("src/main/resources/implicit_scheme_k_j.xlsx");
+            crankNicolsonSchemeKJ = getJK("src/main/resources/crank_nicolson_scheme_k_j.xlsx");
+            implicitSchemeKJ.forEach((key, value) -> implicitSchemeEpsTableRows.add(populateImplicitSchemeEpsTableRow(inputDataDto.toBuilder().J(key).K(value).build())));
+            crankNicolsonSchemeKJ.forEach((key, value) -> crankNicolsonSchemeEpsTableRows.add(populateCrankNicolsonSchemeEpsTableRow(inputDataDto.toBuilder().J(key).K(value).build())));
             implicitSchemeEpsTableObservableRows = FXCollections.observableList(implicitSchemeEpsTableRows);
             crankNicolsonSchemeEpsTableObservableRows = FXCollections.observableList(crankNicolsonSchemeEpsTableRows);
             implicitSchemeTableView.setItems(implicitSchemeEpsTableObservableRows);
@@ -101,37 +91,43 @@ public class EpsTableController implements Initializable, aWindow, InputDataDtoH
         this.inputDataDto = inputDataDto;
     }
 
-    private Pair<ImplicitSchemeEpsTableRow, CrankNicolsonSchemeEpsTableRow> populateRow(InputDataDto inputDataDto) {
+    private ImplicitSchemeEpsTableRow populateImplicitSchemeEpsTableRow(InputDataDto inputDataDto) {
         ImplicitSchemeEpsTableRow implicitSchemeEpsTableRow = new ImplicitSchemeEpsTableRow();
-        CrankNicolsonSchemeEpsTableRow crankNicolsonSchemeEpsTableRow = new CrankNicolsonSchemeEpsTableRow();
         implicitSchemeEpsTableRow.setJ(inputDataDto.getJ());
         implicitSchemeEpsTableRow.setK(inputDataDto.getK());
+
+        ComprehensiveResultDataDto comprehensiveResultDataDto = Calculator.calculate(inputDataDto);
+        BiFunction<Double, Double, Complex> analyticalSolution = comprehensiveResultDataDto.getAnalyticalSolution();
+        implicitSchemeEpsTableRow.setΕ_h_r_h_z(maxEps(comprehensiveResultDataDto.getImplicitSchemeSolution(), comprehensiveResultDataDto.getAnalyticalSolution()));
+
+        comprehensiveResultDataDto = Calculator.calculate(inputDataDto.toBuilder().K(inputDataDto.getK() / 4).J(inputDataDto.getJ() / 2).build());
+        implicitSchemeEpsTableRow.setΕ_2h_r_4h_z(maxEps(comprehensiveResultDataDto.getImplicitSchemeSolution(), analyticalSolution));
+        implicitSchemeEpsTableRow.setΔ_h_r_h_z(implicitSchemeEpsTableRow.getΕ_2h_r_4h_z() / implicitSchemeEpsTableRow.getΕ_h_r_h_z());
+        return implicitSchemeEpsTableRow;
+    }
+
+    private CrankNicolsonSchemeEpsTableRow populateCrankNicolsonSchemeEpsTableRow(InputDataDto inputDataDto) {
+        CrankNicolsonSchemeEpsTableRow crankNicolsonSchemeEpsTableRow = new CrankNicolsonSchemeEpsTableRow();
         crankNicolsonSchemeEpsTableRow.setJ(inputDataDto.getJ());
         crankNicolsonSchemeEpsTableRow.setK(inputDataDto.getK());
 
         ComprehensiveResultDataDto comprehensiveResultDataDto = Calculator.calculate(inputDataDto);
         BiFunction<Double, Double, Complex> analyticalSolution = comprehensiveResultDataDto.getAnalyticalSolution();
-        implicitSchemeEpsTableRow.setΕ_h_r_h_z(maxEps(comprehensiveResultDataDto.getImplicitSchemeSolution(), comprehensiveResultDataDto.getAnalyticalSolution()));
         crankNicolsonSchemeEpsTableRow.setΕ_h_r_h_z(maxEps(comprehensiveResultDataDto.getCrankNicolsonSchemeSolution(), comprehensiveResultDataDto.getAnalyticalSolution()));
 
-        comprehensiveResultDataDto = Calculator.calculate(inputDataDto.toBuilder().K(inputDataDto.getK()/4).J(inputDataDto.getJ()/2).build());
-        implicitSchemeEpsTableRow.setΕ_2h_r_4h_z(maxEps(comprehensiveResultDataDto.getImplicitSchemeSolution(), analyticalSolution));
-
-        comprehensiveResultDataDto = Calculator.calculate(inputDataDto.toBuilder().K(inputDataDto.getK()/2).J(inputDataDto.getJ()/2).build());
+        comprehensiveResultDataDto = Calculator.calculate(inputDataDto.toBuilder().K(inputDataDto.getK() / 2).J(inputDataDto.getJ() / 2).build());
         crankNicolsonSchemeEpsTableRow.setΕ_2h_r_2h_z(maxEps(comprehensiveResultDataDto.getCrankNicolsonSchemeSolution(), analyticalSolution));
-
-        implicitSchemeEpsTableRow.setΔ_h_r_h_z(implicitSchemeEpsTableRow.getΕ_2h_r_4h_z()/implicitSchemeEpsTableRow.getΕ_h_r_h_z());
-        crankNicolsonSchemeEpsTableRow.setΔ_h_r_h_z(crankNicolsonSchemeEpsTableRow.getΕ_2h_r_2h_z()/crankNicolsonSchemeEpsTableRow.getΕ_h_r_h_z());
-        return new Pair<>(implicitSchemeEpsTableRow, crankNicolsonSchemeEpsTableRow);
+        crankNicolsonSchemeEpsTableRow.setΔ_h_r_h_z(crankNicolsonSchemeEpsTableRow.getΕ_2h_r_2h_z() / crankNicolsonSchemeEpsTableRow.getΕ_h_r_h_z());
+        return crankNicolsonSchemeEpsTableRow;
     }
 
     private double maxEps(Matrix<Complex> schemeSolution, BiFunction<Double, Double, Complex> analyticalSolution) {
         double max = 0;
-        double h_r = R/(schemeSolution.getCountRows()-1);
-        double h_z = L/(schemeSolution.getCountColumns()-1);
+        double h_r = R / (schemeSolution.getCountRows() - 1);
+        double h_z = L / (schemeSolution.getCountColumns() - 1);
         for (int j = 0; j < schemeSolution.getCountRows(); j++) {
             for (int k = 0; k < schemeSolution.getCountColumns(); k++) {
-                double temp = schemeSolution.get(j + 1, k + 1).get().subtract(analyticalSolution.apply(j*h_r,k*h_z)).abs();
+                double temp = schemeSolution.get(j + 1, k + 1).get().subtract(analyticalSolution.apply(j * h_r, k * h_z)).abs();
                 if (temp > max) {
                     max = temp;
                 }
@@ -140,24 +136,26 @@ public class EpsTableController implements Initializable, aWindow, InputDataDtoH
         return max;
     }
 
-    private static Map<Integer, Integer> getKJ(){
+    private static Map<Integer, Integer> getJK(String source) {
         FileInputStream file = null;
         try {
-            file = new FileInputStream(new File("src/main/resources/k_j.xlsx"));
+            file = new FileInputStream(new File(source));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         XSSFWorkbook workbook = null;
         try {
+            assert file != null;
             workbook = new XSSFWorkbook(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        assert workbook != null;
         XSSFSheet sheet = workbook.getSheetAt(0);
         Map<Integer, Integer> kj = new TreeMap<>();
         Iterator<Row> iterator = sheet.iterator();
         iterator.next();
-        iterator.forEachRemaining(row -> kj.put((int)row.getCell(0).getNumericCellValue(),(int)row.getCell(1).getNumericCellValue()));
+        iterator.forEachRemaining(row -> kj.put((int) row.getCell(0).getNumericCellValue(), (int) row.getCell(1).getNumericCellValue()));
         return kj;
     }
 }
